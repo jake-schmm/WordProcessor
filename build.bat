baec92518b7dfac7639614e992841d632a29fa8f
jake-schmm <jake.schmm@gmail.com>
2023-10-28 13:29:49 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-cd C:\xampp\mysql\bin
-mysql -u root -p wordprocessordb < C:\xampp\htdocs\swe-6623-word-processor\wordprocessordb.sql
+cd C:\xampp3\mysql\bin
+mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS wordprocessordb;"
+mysql -u root -p wordprocessordb < C:\xampp3\htdocs\swe-6623-word-processor\wordprocessordb.sql

6db36631ba708387f103978dabaae0e849ceb01b
jake-schmm <jake.schmm@gmail.com>
2023-11-18 14:48:53 -0500

    Updated build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 cd C:\xampp\mysql\bin
+mysql -u root -p -e "DROP DATABASE IF EXISTS wordprocessordb;"
 mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS wordprocessordb;"
 mysql -u root -p wordprocessordb < C:\xampp\htdocs\swe-6623-word-processor\wordprocessordb.sql
